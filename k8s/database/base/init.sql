fbdd68264b3fda83ce7e0ab506091ef555c7470f
shl0501 <shl050105010501@gmail.com>
2026-04-03 04:06:55 +0900

    feat(be): 메ㅑ

--- k8s/database/base/init.sql
+++ k8s/database/base/init.sql
@@ -70,10 +70,10 @@ CREATE TABLE IF NOT EXISTS user_subscribe (
 
 CREATE TABLE IF NOT EXISTS environment (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    gem VARCHAR(8192),
-    `invoke` VARCHAR(8192),
-    secret VARCHAR(8192),
-    `map` VARCHAR(8192),
-    `back` VARCHAR(8192),
+    gem TEXT,
+    `invoke` TEXT,
+    secret TEXT,
+    `map` TEXT,
+    `back` TEXT,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
